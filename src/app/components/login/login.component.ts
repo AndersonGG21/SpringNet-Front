6550e63e0d1e534efa753947e09b5dc6e55c3637
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-17 21:25:06 -0500

    Add Login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  toggle(): void {
+      const pInput = <HTMLInputElement>document.getElementById("password");
+      pInput.type == "password"
+        ? (pInput.type = "text")
+        : (pInput.type = "password");
+      const btn = <HTMLButtonElement>document.getElementById("btnToggle");
+
+      btn.classList.toggle("active");
+
+      btn.classList.contains("active")
+        ? (btn.innerHTML = '<i class="bx bx-hide bx-xs"></i>')
+        : (btn.innerHTML = '<i class="bx bx-show bx-xs"></i>')
+  }
 }
