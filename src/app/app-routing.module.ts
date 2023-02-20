7fb53505968cfa1c780dd7d95209d7473e23fb6b
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-19 22:43:19 -0500

    Remove navbar path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
-  {path: "feed", component: FeedComponent}
+  {path: "feed", component: FeedComponent},
+  // {path: "navbar", component: NavbarComponent}
 ];
 
 @NgModule({
