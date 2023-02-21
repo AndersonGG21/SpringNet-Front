a9250e15d33fa39071eb2f454639c19d9d4e3d8c
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-20 22:08:33 -0500

    Add a new path for test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsCardComponent } from './components/posts-card/posts-card.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
-  {path: "feed", component: FeedComponent}
+  {path: "feed", component: FeedComponent},
+  {path: "section", component: PostsCardComponent}
 ];
 
 @NgModule({
