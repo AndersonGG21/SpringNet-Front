868278c48476d9f13e2c1941fb54e3d322be94c4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-21 13:31:03 -0500

    Update feed component

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -25,19 +25,30 @@ export class FeedComponent {
   changeColumns = (numColumns: number): void => {
     const section = document.querySelector("section") as HTMLElement;
     section.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
+
+    if (numColumns == 1) {
+      section.style.width = '500px';
+    }else {
+      section.style.width = 'auto';
+    }
+
     section.style.transition = "all 20s ease-in-out 20s";
   };
 
-  show2Colums() :void{
+  show2Columns() :void{
     this.changeColumns(2);
   }
 
   show3Columns() :void{
     this.changeColumns(3);
+
   }
 
   show1Column() : void {
     this.changeColumns(1);
+    const section = document.querySelector("section") as HTMLElement;
+    section.style.width = '500px';
+    section.style.transition = "all 20s ease-in-out 20s";
   }
 }
 
