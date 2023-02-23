98a3c42b53fb81313811aeee4c022375e041b5e0
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-23 13:33:56 -0500

    Remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsCardComponent } from './components/posts-card/posts-card.component';
 
 const routes: Routes = [
