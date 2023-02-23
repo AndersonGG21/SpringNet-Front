69a906330dd27a28f95c200389c3dba1439c05c6
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-22 21:54:28 -0500

    Refactor login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -22,23 +22,6 @@ export class LoginService {
 
   constructor(private http : HttpClient) { }
 
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
-  }
-
-
-
   login(login : Login){
     this.http.post(this.loginUrl, login, {observe : 'response'}).subscribe(
       response => {
