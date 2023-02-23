04c5af590007c52b65ce389cb7354ab3cb025a80
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-22 21:19:44 -0500

    feat: Create upload img method

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,71 +1,94 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MenuItem } from 'primeng/api';
+import { MediaService } from 'src/app/services/media.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
 })
-
 export class FeedComponent {
-
   cities: City[];
   items: MenuItem[] = [];
   displayModal: boolean | undefined;
   uploadedFiles: any[] = [];
+  url: string = '';
 
   selectedCity: City | undefined;
-font: any;
+  font: any;
+  post !: Post;
+  content : string = '';
 
-  constructor() {
-      this.cities = [
-          {name: 'New York', code: 'NY'},
-          {name: 'Rome', code: 'RM'},
-          {name: 'London', code: 'LDN'},
-          {name: 'Istanbul', code: 'IST'},
-          {name: 'Paris', code: 'PRS'}
-      ];
+  constructor(private mediaService: MediaService) {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' },
+    ];
 
-      this.items = [
-        {label: 'Post', icon: 'pi pi-fw pi-hashtag'},
-        {label: 'Story', icon: 'pi pi-fw pi-history'}
+    this.items = [
+      { label: 'Post', icon: 'pi pi-fw pi-hashtag' },
+      { label: 'Story', icon: 'pi pi-fw pi-history' },
     ];
   }
 
-  onUpload(event: { files: any; }) {
-    for(let file of event.files) {
-        this.uploadedFiles.push(file);
+  upload(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      this.mediaService.uploadFile(formData).subscribe((res) => {
+        this.url = res.url;
+        console.log(this.url);
+      });
     }
-  // ]this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});]
   }
 
   changeColumns = (numColumns: number): void => {
-    const section = document.querySelector("section") as HTMLElement;
+    const section = document.querySelector('section') as HTMLElement;
     section.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
   };
 
-  show2Columns() :void{
+  show2Columns(): void {
     this.changeColumns(2);
   }
 
-  show3Columns() :void{
+  show3Columns(): void {
     this.changeColumns(3);
-
   }
 
-  show1Column() : void {
+  show1Column(): void {
     this.changeColumns(1);
-    const section = document.querySelector("section") as HTMLElement;
+    const section = document.querySelector('section') as HTMLElement;
     section.style.width = '500px';
-    section.style.transition = "all 20s ease-in-out 20s";
+    section.style.transition = 'all 20s ease-in-out 20s';
   }
 
   showModalDialog() {
     this.displayModal = true;
   }
+
+  createPost(){
+    this.post.content = this.content;
+    this.post.image = this.url;
+    this.post.user.id = 12;
+  }
 }
 
 interface City {
-  name: string,
-  code: string
+  name: string;
+  code: string;
+}
+
+interface Post {
+  content: string;
+  image: string;
+  user: {
+    id: number
+  }
 }
