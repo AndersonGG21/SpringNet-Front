08d33adf1d477a3fbb861f0067dc60b6b66a6b72
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-23 20:53:53 -0500

    feat: Add method to get post from API

--- src/app/components/posts-card/posts-card.component.ts
+++ src/app/components/posts-card/posts-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Post } from 'src/app/models/login.model';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-posts-card',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class PostsCardComponent {
   items : number[] = [1,2,3,4,5,6,7,8 ];
+  posts : Post[] = [];
 
+  constructor(private postService : PostService){
+    postService.getPost(12).subscribe((response) => {
+      this.posts = response;
+      console.log(this.posts);
+    })
+  };
 
 }
