44d8c9d1073188cc1a7017bfc6acb1eda849c66c
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-24 14:37:33 -0500

    Add Profile route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
 import { PostsCardComponent } from './components/posts-card/posts-card.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "feed", component: FeedComponent},
-  {path: "section", component: PostsCardComponent}
+  {path: "section", component: PostsCardComponent},
+  {path: "profile", component: ProfileComponent}
 ];
 
 @NgModule({
