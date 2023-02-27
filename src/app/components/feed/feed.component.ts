53a8ca7452a85e8bdd035a3118cfec4e81e6e374
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-26 22:11:12 -0500

    Remove grid functions

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -49,26 +49,6 @@ export class FeedComponent {
     }
   }
 
-  changeColumns = (numColumns: number): void => {
-    const section = document.querySelector('section') as HTMLElement;
-    section.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
-  };
-
-  show2Columns(): void {
-    this.changeColumns(2);
-  }
-
-  show3Columns(): void {
-    this.changeColumns(3);
-  }
-
-  show1Column(): void {
-    this.changeColumns(1);
-    const section = document.querySelector('section') as HTMLElement;
-    section.style.width = '500px';
-    section.style.transition = 'all 20s ease-in-out 20s';
-  }
-
   showModalDialog() {
     this.displayModal = true;
   }
