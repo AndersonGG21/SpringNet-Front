7969bfe6467e1622e3245f7eba806fe49f7dcff2
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-01 21:53:53 -0500

    Add follow service

---
+++ src/app/services/follow.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowService {
+
+  private baseUrl : string = 'http://localhost:8080/api/follows';
+  private options = {
+    observe: 'response' as const,
+    headers : new HttpHeaders()
+    .set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRlcnNvbmdhcmNlc2dhcmNpYUBnbWFpbC5jb20iLCJleHAiOjE2NzkyODYwMTIsIm5hbWUiOiJhbmRlci5fLmdnIn0.rRajQFoLIbpux5JUYNg9rLcBCe3oZs7w_ihQAuNv3t_X5CRIo24qrTLw9McOOoyuRfyNGFbvwnpo3Msliy6uEQ')
+  };
+
+  constructor(private http : HttpClient) { }
+
+
+  getCountOfFollowers(): Observable<number>{
+    return this.http.get<number>(`${this.baseUrl}/count-followers`, {headers : this.options.headers});
+  }
+}
