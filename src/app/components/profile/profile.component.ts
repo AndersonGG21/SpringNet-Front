e032885236fb7cbc8faa301ecf7eb4001949dd61
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-01 21:53:43 -0500

    Add method to get number of followers

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/login.model';
+import { FollowService } from 'src/app/services/follow.service';
 import { PostService } from 'src/app/services/post.service';
 
 @Component({
@@ -10,13 +11,18 @@ import { PostService } from 'src/app/services/post.service';
 export class ProfileComponent implements OnInit{
 
   userPosts : Post[] = [];
+  followers : number = 0;
 
-  constructor( private postService : PostService){}
+  constructor( private postService : PostService, private followService : FollowService){}
 
   ngOnInit(): void {
     this.postService.getPostByUser(12).subscribe((response) => {
       this.userPosts = response
     })
+
+    this.followService.getCountOfFollowers().subscribe((response) => {
+      this.followers = response;
+    });
   };
 
 
