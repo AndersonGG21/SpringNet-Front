8d96e335de6279dcb3d37541fbcd257325e1616d
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-03 22:55:12 -0500

    Add get user profile method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl : string = 'http://localhost:8080/api/users';
+  private options = {
+    observe: 'response' as const,
+    headers : new HttpHeaders()
+    .set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRlcnNvbmdhcmNlc2dhcmNpYUBnbWFpbC5jb20iLCJleHAiOjE2NzkyODYwMTIsIm5hbWUiOiJhbmRlci5fLmdnIn0.rRajQFoLIbpux5JUYNg9rLcBCe3oZs7w_ihQAuNv3t_X5CRIo24qrTLw9McOOoyuRfyNGFbvwnpo3Msliy6uEQ')
+  };
+
+  constructor(private http : HttpClient) { }
+
+  getUserProfile() : Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/12`, {headers : this.options.headers});
+  }
 }
