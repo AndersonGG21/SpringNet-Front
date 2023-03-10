ccb1a3a7849fcd1a5cb0da0c0470b041dba65495
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-09 21:47:41 -0500

    Add http options

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Login } from '../models/types';
-
+import { Login, User } from '../models/types';
 
 
 @Injectable({
@@ -10,15 +9,13 @@ import { Login } from '../models/types';
 export class LoginService {
 
   private loginUrl  = "http://localhost:8080/login";
+  private usersUrl = "http://localhost:8080/api/users"
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json' ,
-      Authorization : ''
-    },),
-    observe: 'response' as const
+  private options = {
+    observe: 'response' as const,
+    headers : new HttpHeaders()
+    .set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRlcnNvbmdhcmNlc2dhcmNpYUBnbWFpbC5jb20iLCJleHAiOjE2NzkyODYwMTIsIm5hbWUiOiJhbmRlci5fLmdnIn0.rRajQFoLIbpux5JUYNg9rLcBCe3oZs7w_ihQAuNv3t_X5CRIo24qrTLw9McOOoyuRfyNGFbvwnpo3Msliy6uEQ')
   };
-
   constructor(private http : HttpClient) { }
 
   login(login : Login){
@@ -27,5 +24,10 @@ export class LoginService {
         console.log(response.headers.get("Authorization"));
       }
     );
+
+    this.http.get<User>(`${this.usersUrl}/by-email/${login.email}`,{headers : this.options.headers}).subscribe((response) => {
+      console.log(response);
+    })
   }
+
 }
