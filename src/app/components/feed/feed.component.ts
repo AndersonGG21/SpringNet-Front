6b66f27d9458309e1b374770b66f4fce77024235
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-24 14:35:55 -0500

    Change post service

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 import { MenuItem } from 'primeng/api';
 import { timeInterval } from 'rxjs';
 import { Post } from 'src/app/models/types';
 import { PostDataBehaviorSubjectService } from 'src/app/services/post-data-behavior-subject.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-feed',
@@ -17,13 +19,13 @@ export class FeedComponent {
   posts : Post[] = [];
   time = 0;
 
-  constructor(private postDataB : PostDataBehaviorSubjectService, private title : Title) {
+  constructor(private postDataB : PostService, private title : Title, private cookie : CookieService) {
     this.items = [
       { label: 'Post', icon: 'pi pi-fw pi-hashtag' },
       { label: 'Story', icon: 'pi pi-fw pi-history' },
     ];
 
-    this.postDataB.posts.pipe(timeInterval()).subscribe((posts) => {
+    this.postDataB.getPost(Number(cookie.get("uuid"))).pipe(timeInterval()).subscribe((posts) => {
       this.time = posts.interval;
 
       const skeleton = document.getElementById("skeleton-loader") as HTMLElement;
