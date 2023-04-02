5090c3177d17ec17b3b0c743ff738578f2a97d85
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-01 22:07:35 -0500

    Add get all users method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -21,4 +21,8 @@ export class UserService {
   getUserProfile(id : number) : Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/${id}`, {headers : this.options.headers});
   }
+
+  getAllUsers() : Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl, {headers : this.options.headers})
+  }
 }
