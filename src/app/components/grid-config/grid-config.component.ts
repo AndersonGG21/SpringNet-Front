204dc985577fe8e75fdd8fe596f591310702accf
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-01 22:06:55 -0500

    Refactor code

--- src/app/components/grid-config/grid-config.component.ts
+++ src/app/components/grid-config/grid-config.component.ts
@@ -31,6 +31,13 @@ export class GridConfigComponent {
     section.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
   };
 
+  show1Column(): void {
+    this.changeColumns(1);
+    const section = document.querySelector('section') as HTMLElement;
+    section.style.width = '500px';
+    section.style.transition = 'all 20s ease-in-out 20s';
+  }
+
   show2Columns(): void {
     const section = document.querySelector('section') as HTMLElement;
     section.style.width = '400px';
@@ -39,13 +46,6 @@ export class GridConfigComponent {
 
   show3Columns(): void {
     this.changeColumns(3);
-  }
-
-  show1Column(): void {
-    this.changeColumns(1);
-    const section = document.querySelector('section') as HTMLElement;
-    section.style.width = '500px';
-    section.style.transition = 'all 20s ease-in-out 20s';
   }
 
   showModalDialog() {
