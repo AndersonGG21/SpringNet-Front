8518076686d4dd52fad56bdb157e7c5034413581
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-03 22:21:12 -0500

    Add follow button validations

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component, Injectable, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import { Post, User } from 'src/app/models/types';
+import { Button } from 'primeng/button';
+import { Follow, Post, User } from 'src/app/models/types';
 import { FollowService } from 'src/app/services/follow.service';
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
@@ -20,12 +21,24 @@ export class ProfileComponent implements OnInit{
   userId = 0;
   user !: User;
   flag = false;
+  checkFollow = false;
+  follow : Follow = {
+    follower : {
+      id: Number(this.cookie.get('uuid'))
+    },
+    following : {
+      id: this.userId
+    }
+  }
 
   constructor( private postService : PostService, private followService : FollowService, private userService : UserService, private title : Title, private route : ActivatedRoute, private cookie : CookieService){}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params : ParamMap) => {
+
       this.userId = Number(params.get('id'));
+      this.follow.following.id = this.userId;
+
       Number(this.cookie.get('uuid')) == this.userId ? this.flag = false : this.flag = true;
 
       this.userService.getUserProfile(this.userId).subscribe((response) => {
@@ -45,6 +58,24 @@ export class ProfileComponent implements OnInit{
         this.following = response;
       });
     })
+
+    this.followService.checkFollow(this.follow).subscribe((response) => {
+      response >= 1 ? this.checkFollow = true : this.checkFollow = false;
+    });
   }
 
+  setFollow() : void {
+    this.followService.setFollow(this.follow).subscribe();
+    this.checkFollow = !this.checkFollow;
+  }
+
+  deleteText() : void {
+    const button = document.querySelector('.follow-btn') as HTMLButtonElement;
+    button.textContent = '';
+  }
+
+  addText() : void {
+    const button = document.querySelector('.follow-btn') as HTMLButtonElement;
+    button.textContent = 'Following';
+  }
 }
