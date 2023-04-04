9ec8021d8dd6302e5fff44209953cbb7cc4125a6
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-03 22:21:40 -0500

    Add set follow and check follow methods

--- src/app/services/follow.service.ts
+++ src/app/services/follow.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
+import { Follow } from '../models/types';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,13 @@ export class FollowService {
 
   getCountOfFollowing(id : number): Observable<number>{
     return this.http.get<number>(`${this.baseUrl}/count-following/${id}`, {headers : this.options.headers});
+  }
+
+  setFollow(follow : Follow) : Observable<any> {
+    return this.http.post<Follow>(`${this.baseUrl}/follow-user`, follow, {headers : this.options.headers})
+  }
+
+  checkFollow(follow : Follow) : Observable<number> {
+    return this.http.post<any>(`${this.baseUrl}/check-follow`, follow ,{headers : this.options.headers});
   }
 }
