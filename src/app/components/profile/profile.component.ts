20e0657062f594c4538227d94d4ed72bdf1cbcc8
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-03 20:21:23 -0500

    Refactor load profile method

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Post, User } from 'src/app/models/types';
 import { FollowService } from 'src/app/services/follow.service';
 import { PostService } from 'src/app/services/post.service';
@@ -16,31 +17,33 @@ export class ProfileComponent implements OnInit{
   userPosts : Post[] = [];
   followers  = 0;
   following  = 0;
-  userId : number = this.route.snapshot.params['id'];
+  userId = 0;
   user !: User;
 
   constructor( private postService : PostService, private followService : FollowService, private userService : UserService, private title : Title, private route : ActivatedRoute){}
 
   ngOnInit(): void {
-    this.postService.getPostByUser(this.userId).subscribe((response) => {
-      this.userPosts = response
+
+    this.route.paramMap.subscribe((params : ParamMap) => {
+      this.userId = Number(params.get('id'));
+
+      this.userService.getUserProfile(this.userId).subscribe((response) => {
+        this.user = response;
+        this.title.setTitle(`@${this.user.username} | Pics and Videos`);
+      })
+
+      this.postService.getPostByUser(this.userId).subscribe((response) => {
+        this.userPosts = response
+      })
+
+      this.followService.getCountOfFollowers(this.userId).subscribe((response) => {
+        this.followers = response;
+      });
+
+      this.followService.getCountOfFollowing(this.userId).subscribe((response) => {
+        this.following = response;
+      });
     })
-
-    this.followService.getCountOfFollowers(this.userId).subscribe((response) => {
-      this.followers = response;
-    });
-
-    this.followService.getCountOfFollowing(this.userId).subscribe((response) => {
-      this.following = response;
-    });
-
-    this.userService.getUserProfile(this.userId).subscribe((response) => {
-      this.user = response;
-      this.title.setTitle(`@${this.user.username} | Pics and Videos`);
-    })
-
   }
-
-
 
 }
