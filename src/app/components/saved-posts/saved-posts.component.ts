30102abfd4c0c82debf8f0ca35393b153d2cc397
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-05 21:48:40 -0500

    Get saved posts

--- src/app/components/saved-posts/saved-posts.component.ts
+++ src/app/components/saved-posts/saved-posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { Post } from 'src/app/models/types';
 import { PostService } from 'src/app/services/post.service';
@@ -11,14 +13,18 @@ import { PostService } from 'src/app/services/post.service';
 export class SavedPostsComponent implements OnInit {
   posts : Post[] = [];
 
-  constructor(private postService : PostService, private cookie : CookieService){}
+  constructor(private postService : PostService, private cookie : CookieService, private aRoute : ActivatedRoute, private title : Title){}
 
   ngOnInit(): void {
-    this.postService.getSavedPosts(Number(this.cookie.get('uuid'))).subscribe((response) => {
-      for (let index = 0; index < response.length; index++) {
-        this.posts.push(response[index].post)
-      }
+    this.aRoute.paramMap.subscribe((params : ParamMap) => {
+      this.postService.getSavedPosts(Number(params.get('id'))).subscribe((response) => {
+        this.title.setTitle(`@${response[0].user.username} | Saved Posts`);
+        for (let index = 0; index < response.length; index++) {
+          this.posts.push(response[index].post)
+        }
+      })
     })
+
   };
 
 
