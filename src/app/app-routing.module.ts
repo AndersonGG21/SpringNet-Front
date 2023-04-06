8408d6cd7749cb79667c6a3c3229dd1e6b6bb938
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-05 21:05:00 -0500

    Add Saved Post route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
 import { PostsCardComponent } from './components/posts-card/posts-card.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SavedPostsComponent } from './components/saved-posts/saved-posts.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "feed", component: FeedComponent},
   {path: "section", component: PostsCardComponent},
-  {path: "profile/:id", component: ProfileComponent}
+  {path: "profile/:id", component: ProfileComponent},
+  {path: "profile/saved/:id", component: SavedPostsComponent}
 ];
 
 @NgModule({
