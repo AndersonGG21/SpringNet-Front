2f02a00374bbb75735ac6b353e28bc09aad48208
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-07 22:00:19 -0500

    Get stories from db

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StoryService } from 'src/app/services/story.service';
 
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.css']
 })
-export class StoriesComponent {
+
+export class StoriesComponent implements OnInit {
+  stories : any;
+  private storieService = inject(StoryService)
+
+
+  ngOnInit(): void {
+    this.storieService.getStories().subscribe(response => {
+      this.stories = response;
+      console.log(this.stories);
+    })
+  }
 
 }
---
+++ src/app/services/story.service.ts
@@ -0,0 +1,26 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoryService {
+
+  baseUrl  = 'http://localhost:8080/api/stories'
+
+  private options = {
+    observe: 'response' as const,
+    headers : new HttpHeaders()
+    .set('Authorization', `Bearer ${this.cookie.get("Bearer")}`)
+    .set('Access-Control-Allow-Origin', '*')
+    .set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  };
+  constructor(private http : HttpClient, private cookie : CookieService) { }
+
+  getStories() : Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${Number(this.cookie.get('uuid'))}`, {headers: this.options.headers})
+  }
+
+}
