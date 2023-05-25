57c5263a6f7592342f14c0f81817ddc408955d4a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-05-25 14:37:42 -0500

    Remove authorization header

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -15,7 +15,6 @@ export class LoginService {
   private options = {
     observe: 'response' as const,
     headers : new HttpHeaders()
-    .set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRlcnNvbmdhcmNlc2dhcmNpYUBnbWFpbC5jb20iLCJleHAiOjE2NzkyODYwMTIsIm5hbWUiOiJhbmRlci5fLmdnIn0.rRajQFoLIbpux5JUYNg9rLcBCe3oZs7w_ihQAuNv3t_X5CRIo24qrTLw9McOOoyuRfyNGFbvwnpo3Msliy6uEQ')
   };
   constructor(private http : HttpClient, private cookie : CookieService) { }
 
@@ -33,7 +32,8 @@ export class LoginService {
     );
 
     this.http.get<User>(`${this.usersUrl}/by-email/${login.email}`,{headers : this.options.headers}).subscribe((response) => {
-      console.log(response);
+      // console.log(response);
+      console.log(response.id);
       const uuid = response.id;
       this.cookie.set("uuid", String(uuid));
     })
