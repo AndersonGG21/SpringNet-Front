0f5b82a41426b1311720c04758ca9affbf54ddb5
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-06-27 18:49:47 -0500

    Configure Web Socket protocol

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,15 +1,165 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { SocketService } from 'src/app/services/socket.service';
+import { ChatMessage, User } from 'src/app/models/types';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.userService.getAllUsers().subscribe((response) => {
+      this.users = response;
+    })
   }
 
+  users: User[] = [];
+  filteredUsers : User[] = [];
+  selectedUser : User | undefined;
+  openSearchInput = false;
 
+  constructor(private socketService: SocketService, private userService : UserService, private router : Router) {}
+
+  ngAfterViewInit(): void {
+    this.chatPage = document.querySelector('#chat-page') as HTMLElement;
+    this.usernameForm = document.querySelector(
+      '#usernameForm'
+    ) as HTMLFormElement;
+    this.messageForm = document.querySelector(
+      '#messageForm'
+    ) as HTMLFormElement;
+    this.messageInput = document.querySelector('#message') as HTMLInputElement;
+    this.messageArea = document.querySelector('#messageArea') as HTMLElement;
+    this.connectingElement = document.querySelector(
+      '.connecting'
+    ) as HTMLElement;
+    this.usernamePage = document.querySelector(
+      '#username-page1'
+    ) as HTMLElement;
+  }
+
+  usernamePage: any;
+  chatPage: any;
+  usernameForm: any;
+  messageForm: any;
+  messageInput: any;
+  messageArea: any;
+  connectingElement: any;
+  username: string = '';
+  stompClient : any;
+  messages : ChatMessage[] = [];
+
+
+  colors = [
+    '#2196F3',
+    '#32c787',
+    '#00BCD4',
+    '#ff5652',
+    '#ffc107',
+    '#ff85af',
+    '#FF9800',
+    '#39bbb0',
+  ];
+
+  connect(event: any) {
+    let name = document.querySelector('#name') as HTMLInputElement;
+    this.username = name.value.trim();
+
+    if (this.username) {
+      this.usernamePage.classList.add('hidden');
+      this.chatPage.classList.remove('hidden');
+
+      this.socketService.connect()
+      this.connectingElement.classList.add('hidden');
+
+    }
+    event.preventDefault();
+  }
+
+  onConnected() {
+    // this.socketService.onConnected(this.username);
+    this.connectingElement.classList.add('hidden');
+  }
+
+  onError() {
+    this.connectingElement.textContent =
+      'Could not connect to WebSocket server. Please refresh this page to try again!';
+    this.connectingElement.style.color = 'red';
+  }
+
+  sendMessage() {
+    var messageContent = this.messageInput.value.trim();
+    if (messageContent) {
+      var chatMessage = {
+        content: this.messageInput.value,
+        sender: this.username,
+        type: 'CHAT',
+      };
+      // stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+      this.socketService.send(chatMessage);
+      this.messageInput.value = '';
+    }
+    this.messages = this.socketService.getMessagesList();
+    console.log(this.messages)
+  }
+
+  onMessageReceived(payload: any) {
+    // let message = JSON.parse(payload.body);
+
+    // let messageElement = document.createElement('li');
+
+    // if (message.type === 'JOIN') {
+    //   messageElement.classList.add('event-message');
+    //   message.content = message.sender + ' joined!';
+    // } else if (message.type === 'LEAVE') {
+    //   messageElement.classList.add('event-message');
+    //   message.content = message.sender + ' left!';
+    // } else {
+    //   messageElement.classList.add('chat-message');
+
+    //   let avatarElement = document.createElement('i');
+    //   let avatarText = document.createTextNode(message.sender[0]);
+    //   avatarElement.appendChild(avatarText);
+    //   avatarElement.style.backgroundColor = this.getAvatarColor(message.sender);
+
+    //   messageElement.appendChild(avatarElement);
+
+    //   let usernameElement = document.createElement('span');
+    //   let usernameText = document.createTextNode(message.sender);
+    //   usernameElement.appendChild(usernameText);
+    //   messageElement.appendChild(usernameElement);
+    // }
+
+    // let textElement = document.createElement('p');
+    // let messageText = document.createTextNode(message.content);
+    // textElement.appendChild(messageText);
+
+    // messageElement.appendChild(textElement);
+
+    // this.messageArea.appendChild(messageElement);
+    // this.messageArea.scrollTop = this.messageArea.scrollHeight;
+    // this.socketService.onMessageRecieved();
+  }
+
+  filterUsers(event : any) {
+    let filtered : any[] = [];
+    let query = event.query;
+
+    for(let i = 0; i < this.users.length; i++) {
+        let user = this.users[i];
+        if (user.username?.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            filtered.push(user);
+        }
+    }
+
+    this.filteredUsers = filtered;
+  }
+
+  showSelectedUser() : void {
+    const userId = this.selectedUser?.id
+    this.router.navigate([`/profile/${userId}`])
+  }
 }
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,27 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
-import { Observable } from 'rxjs';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+import { ChatMessage } from '../models/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor(private socket : Socket) { }
+  stompClient: any;
+  topic : string = "/topic/public";
+  webSocketPoint : string = "http://localhost:8080/spring-websocket";
+  messages : ChatMessage[] = [];
 
-  connect(username : String ) : void {
-    this.socket.emit('join', username);
+  connect() : void {
+    console.log("Intialize WebSocket Connection");
+    let ws = SockJS(this.webSocketPoint);
+    this.stompClient = Stomp.over(ws);
+    const _this = this;
+    _this.stompClient.connect({}, function (frame : any){
+      _this.stompClient.subscribe(_this.topic,function (greetingResponse : any){
+        console.log("AQUI RETORNO EL MENSAHE")
+        _this.onMessageRecieved(greetingResponse);
+      })
+    }, this.errorCallBack )
   }
 
   disconnect() : void {
-    this.socket.disconnect();
+    if (this.stompClient != null) {
+      this.stompClient.disconnected();
+    }
+
+    console.log("Disconnected");
   }
 
-  sendMessage(message : String) {
-    this.socket.emit('message', message);
+  send(message : any) {
+    console.log("Sending.....................");
+    this.stompClient.send("/app/chat.senMessage", {}, JSON.stringify(message));
   }
 
-  public onMessageReceived(): Observable<any> {
-    return this.socket.fromEvent('message');
+  onMessageRecieved(message : any) {
+    console.log(`Message recieved from: ${message.body}`);
+    const obj = JSON.parse(message.body);
+    this.messages.push(obj);
   }
+
+  errorCallBack(error : any){
+    console.log("errorCallBack -> " + error)
+  }
+
+  getMessagesList() : ChatMessage[] {
+    return this.messages;
+  }
+
 }
