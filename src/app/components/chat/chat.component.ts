56e8bff61dbb74f9e767714a80fcf82b371d8ab7
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-06-30 21:19:37 -0500

    Remove unused code

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,165 +1,83 @@
-import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, inject } from '@angular/core';
 import { SocketService } from 'src/app/services/socket.service';
 import { ChatMessage, User } from 'src/app/models/types';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit, AfterViewInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
+
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe((response) => {
       this.users = response;
-    })
+    });
   }
 
   users: User[] = [];
-  filteredUsers : User[] = [];
-  selectedUser : User | undefined;
-  openSearchInput = false;
+  filteredUsers: User[] = [];
+  selectedUser: User | undefined;
+  sessionID = 0;
+  userID = this.cookie.get("uuid");
+  messages: ChatMessage[] = [];
+  scroller = 0;
 
-  constructor(private socketService: SocketService, private userService : UserService, private router : Router) {}
+  constructor(
+    private socketService: SocketService,
+    private userService: UserService,
+    private router: Router,
+    private cookie : CookieService
+  ) {}
 
-  ngAfterViewInit(): void {
-    this.chatPage = document.querySelector('#chat-page') as HTMLElement;
-    this.usernameForm = document.querySelector(
-      '#usernameForm'
-    ) as HTMLFormElement;
-    this.messageForm = document.querySelector(
-      '#messageForm'
-    ) as HTMLFormElement;
-    this.messageInput = document.querySelector('#message') as HTMLInputElement;
-    this.messageArea = document.querySelector('#messageArea') as HTMLElement;
-    this.connectingElement = document.querySelector(
-      '.connecting'
-    ) as HTMLElement;
-    this.usernamePage = document.querySelector(
-      '#username-page1'
-    ) as HTMLElement;
+  ngAfterViewChecked(): void {
+    this.scrollDown();
+  }
+  connect(id: any) {
+    this.sessionID = Number(id) + Number(this.cookie.get("uuid"));
+    this.messages = this.socketService.getMessagesList();
+    this.socketService.connect(this.sessionID);
   }
 
-  usernamePage: any;
-  chatPage: any;
-  usernameForm: any;
-  messageForm: any;
-  messageInput: any;
-  messageArea: any;
-  connectingElement: any;
-  username: string = '';
-  stompClient : any;
-  messages : ChatMessage[] = [];
-
-
-  colors = [
-    '#2196F3',
-    '#32c787',
-    '#00BCD4',
-    '#ff5652',
-    '#ffc107',
-    '#ff85af',
-    '#FF9800',
-    '#39bbb0',
-  ];
-
-  connect(event: any) {
-    let name = document.querySelector('#name') as HTMLInputElement;
-    this.username = name.value.trim();
-
-    if (this.username) {
-      this.usernamePage.classList.add('hidden');
-      this.chatPage.classList.remove('hidden');
-
-      this.socketService.connect()
-      this.connectingElement.classList.add('hidden');
-
-    }
-    event.preventDefault();
-  }
-
-  onConnected() {
-    // this.socketService.onConnected(this.username);
-    this.connectingElement.classList.add('hidden');
-  }
-
-  onError() {
-    this.connectingElement.textContent =
-      'Could not connect to WebSocket server. Please refresh this page to try again!';
-    this.connectingElement.style.color = 'red';
-  }
-
-  sendMessage() {
-    var messageContent = this.messageInput.value.trim();
-    if (messageContent) {
-      var chatMessage = {
-        content: this.messageInput.value,
-        sender: this.username,
+  sendMessage(message : string) {
+    if (message) {
+      let chatMessage = {
+        content: message,
+        sender: this.cookie.get("uuid"),
+        sessionID: this.sessionID,
         type: 'CHAT',
       };
-      // stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
       this.socketService.send(chatMessage);
-      this.messageInput.value = '';
     }
+
     this.messages = this.socketService.getMessagesList();
-    console.log(this.messages)
+
   }
 
-  onMessageReceived(payload: any) {
-    // let message = JSON.parse(payload.body);
-
-    // let messageElement = document.createElement('li');
-
-    // if (message.type === 'JOIN') {
-    //   messageElement.classList.add('event-message');
-    //   message.content = message.sender + ' joined!';
-    // } else if (message.type === 'LEAVE') {
-    //   messageElement.classList.add('event-message');
-    //   message.content = message.sender + ' left!';
-    // } else {
-    //   messageElement.classList.add('chat-message');
-
-    //   let avatarElement = document.createElement('i');
-    //   let avatarText = document.createTextNode(message.sender[0]);
-    //   avatarElement.appendChild(avatarText);
-    //   avatarElement.style.backgroundColor = this.getAvatarColor(message.sender);
-
-    //   messageElement.appendChild(avatarElement);
-
-    //   let usernameElement = document.createElement('span');
-    //   let usernameText = document.createTextNode(message.sender);
-    //   usernameElement.appendChild(usernameText);
-    //   messageElement.appendChild(usernameElement);
-    // }
-
-    // let textElement = document.createElement('p');
-    // let messageText = document.createTextNode(message.content);
-    // textElement.appendChild(messageText);
-
-    // messageElement.appendChild(textElement);
-
-    // this.messageArea.appendChild(messageElement);
-    // this.messageArea.scrollTop = this.messageArea.scrollHeight;
-    // this.socketService.onMessageRecieved();
+  scrollDown() {
+    const container = document.querySelector('.chat-text-area') as HTMLDivElement;
+    container.scrollTo({ top: container.scrollHeight, behavior: 'smooth' });
   }
 
-  filterUsers(event : any) {
-    let filtered : any[] = [];
+  filterUsers(event: any) {
+    let filtered: any[] = [];
     let query = event.query;
 
-    for(let i = 0; i < this.users.length; i++) {
-        let user = this.users[i];
-        if (user.username?.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-            filtered.push(user);
-        }
+    for (let i = 0; i < this.users.length; i++) {
+      let user = this.users[i];
+      if (user.username?.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(user);
+      }
     }
 
     this.filteredUsers = filtered;
   }
 
-  showSelectedUser() : void {
-    const userId = this.selectedUser?.id
-    this.router.navigate([`/profile/${userId}`])
+  showSelectedUser(): void {
+    const userId = this.selectedUser?.id;
+    this.router.navigate([`/profile/${userId}`]);
   }
 }
