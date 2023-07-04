eda1e1bc58efa19e934881dbb6f15f37c00d831d
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-04 18:03:02 -0500

    Refactor code

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewChecked, AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { AfterViewChecked,Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SocketService } from 'src/app/services/socket.service';
 import { ChatMessage, User } from 'src/app/models/types';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { Subject, interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -12,19 +13,17 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class ChatComponent implements OnInit, AfterViewChecked {
 
-  ngOnInit(): void {
-    this.userService.getAllUsers().subscribe((response) => {
-      this.users = response;
-    });
-  }
 
   users: User[] = [];
   filteredUsers: User[] = [];
   selectedUser: User | undefined;
   sessionID = 0;
-  userID = this.cookie.get("uuid");
+  username = this.cookie.get("username");
   messages: ChatMessage[] = [];
   scroller = 0;
+  connected = false;
+  onlineUsers : string[] = [];
+  @ViewChild("input") input: ElementRef = {} as ElementRef;
 
   constructor(
     private socketService: SocketService,
@@ -33,20 +32,42 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     private cookie : CookieService
   ) {}
 
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((response) => {
+      this.users = response;
+    });
+
+    this.socketService.getOnlineUsersSubject().subscribe((changes) => {
+      this.onlineUsers.push(changes);
+    })
+
+    this.socketService.connectOnline();
+    setTimeout(() => {
+       this.prueba();
+    }, 3000);
+  }
+
   ngAfterViewChecked(): void {
     this.scrollDown();
   }
-  connect(id: any) {
-    this.sessionID = Number(id) + Number(this.cookie.get("uuid"));
+
+  connect(user: User) {
+    this.sessionID = Number(user.id) + Number(this.cookie.get("uuid"));
     this.messages = this.socketService.getMessagesList();
     this.socketService.connect(this.sessionID);
+    this.selectedUser = user;
+    this.showChatArea();
+
+    setTimeout(() => {
+      this.onConnected();
+    }, 500);
   }
 
   sendMessage(message : string) {
     if (message) {
       let chatMessage = {
         content: message,
-        sender: this.cookie.get("uuid"),
+        sender: this.cookie.get("username"),
         sessionID: this.sessionID,
         type: 'CHAT',
       };
@@ -54,7 +75,7 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     }
 
     this.messages = this.socketService.getMessagesList();
-
+    this.input.nativeElement.value = "";
   }
 
   scrollDown() {
@@ -76,8 +97,52 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.filteredUsers = filtered;
   }
 
-  showSelectedUser(): void {
-    const userId = this.selectedUser?.id;
-    this.router.navigate([`/profile/${userId}`]);
+  showChatArea() : void {
+    const container = document.querySelector(".chat-area") as HTMLElement;
+    const placeholder = document.querySelector(".placeholder") as HTMLElement;
+    container.style.display = "flex";
+    placeholder.style.display = "none";
+    this.onlineUsers.find(username => username == this.selectedUser?.username) ? this.connected = true : this.connected = false;
+  }
+
+  showSelectedUser() : void {
+    const userId = this.selectedUser?.id
+    this.router.navigate([`/profile/${userId}`])
+  }
+
+  onConnected() : void{
+    let chatMessage = {
+      content: "",
+      sender: this.cookie.get("username"),
+      sessionID: this.sessionID,
+      type: 'JOIN',
+    };
+    this.socketService.send(chatMessage);
+  }
+
+  prueba() : void {
+    let chatMessage = {
+      content: "",
+      sender: this.cookie.get("username"),
+      sessionID: this.sessionID,
+      type: 'JOIN',
+    };
+    this.socketService.sendConnection(chatMessage);
+  }
+
+  onDisconnected() : void {
+    let chatMessage = {
+      content: "",
+      sender: this.cookie.get("username"),
+      sessionID: this.sessionID,
+      type: 'LEAVE',
+    };
+    this.socketService.send(chatMessage);
+    this.socketService.disconnect();
+    const container = document.querySelector(".chat-area") as HTMLElement;
+    const placeholder = document.querySelector(".placeholder") as HTMLElement;
+    container.style.display = "none";
+    placeholder.style.display = "flex";
+
   }
 }
