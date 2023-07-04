a4de9a1ce90f21006daa579a08e739d170c0cba1
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-04 18:09:20 -0500

    Add documentation

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -51,6 +51,13 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.scrollDown();
   }
 
+  /**
+   * The connect function establishes a connection with a user, sets the session ID, retrieves the
+   * messages list, connects to the socket service, selects a user, shows the chat area, and triggers
+   * the onConnected function after a delay.
+   * @param {User} user - The `user` parameter is an object that represents the user who is connecting.
+   * It likely contains information such as the user's ID, name, and other relevant details.
+   */
   connect(user: User) {
     this.sessionID = Number(user.id) + Number(this.cookie.get("uuid"));
     this.messages = this.socketService.getMessagesList();
@@ -63,6 +70,11 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     }, 500);
   }
 
+  /**
+   * The function sends a chat message using a socket service and updates the messages list.
+   * @param {string} message - The `message` parameter is a string that represents the content of the
+   * message that will be sent.
+   */
   sendMessage(message : string) {
     if (message) {
       let chatMessage = {
@@ -78,11 +90,21 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.input.nativeElement.value = "";
   }
 
+  /**
+   * The scrollDown function scrolls to the bottom of a chat text area with a smooth animation.
+   */
   scrollDown() {
     const container = document.querySelector('.chat-text-area') as HTMLDivElement;
     container.scrollTo({ top: container.scrollHeight, behavior: 'smooth' });
   }
 
+  /**
+   * The function filters an array of users based on a query string and assigns the filtered users to a
+   * variable.
+   * @param {any} event - The event parameter is an object that contains information about the event
+   * that triggered the filtering. It could include properties such as the query string entered by the
+   * user.
+   */
   filterUsers(event: any) {
     let filtered: any[] = [];
     let query = event.query;
@@ -97,6 +119,10 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.filteredUsers = filtered;
   }
 
+  /**
+   * The function `showChatArea()` displays the chat area, hides the placeholder, and checks if the
+   * selected user is online.
+   */
   showChatArea() : void {
     const container = document.querySelector(".chat-area") as HTMLElement;
     const placeholder = document.querySelector(".placeholder") as HTMLElement;
@@ -105,11 +131,17 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.onlineUsers.find(username => username == this.selectedUser?.username) ? this.connected = true : this.connected = false;
   }
 
+  /**
+   * The function navigates to the profile page of the selected user.
+   */
   showSelectedUser() : void {
     const userId = this.selectedUser?.id
     this.router.navigate([`/profile/${userId}`])
   }
 
+  /**
+   * The function sends a JOIN message to the socket service with the sender's username and session ID.
+   */
   onConnected() : void{
     let chatMessage = {
       content: "",
@@ -120,6 +152,10 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.socketService.send(chatMessage);
   }
 
+  /**
+   * The function sends a connection message to a socket service with the sender's username, session
+   * ID, and a type of 'JOIN'.
+   */
   onlineConnectionMessage() : void {
     let chatMessage = {
       content: "",
@@ -130,6 +166,10 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     this.socketService.sendConnection(chatMessage);
   }
 
+  /**
+   * The function `onDisconnected` sends a leave message to the server, disconnects the socket
+   * connection, and hides the chat area while displaying a placeholder.
+   */
   onDisconnected() : void {
     let chatMessage = {
       content: "",
@@ -143,6 +183,5 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     const placeholder = document.querySelector(".placeholder") as HTMLElement;
     container.style.display = "none";
     placeholder.style.display = "flex";
-
   }
 }
