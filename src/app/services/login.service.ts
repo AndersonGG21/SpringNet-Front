8f96e70ab74ceb25424b7b9a6fe6bcba04429535
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-06 18:12:08 -0500

    Refactor login method

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service/public-api';
 import { Login, User } from '../models/types';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -11,6 +12,7 @@ export class LoginService {
 
   private loginUrl  = "http://localhost:8080/login";
   private usersUrl = "http://localhost:8080/api/users"
+  private router = inject(Router);
 
   private options = {
     observe: 'response' as const,
@@ -28,16 +30,17 @@ export class LoginService {
           this.cookie.set("Bearer", token.replace("Bearer", "").trim());
         }
 
+      }, error => {
+        alert("Wrong username or password");
+      }, () => {
+        this.http.get<User>(`${this.usersUrl}/by-email/${login.email}`,{headers : this.options.headers}).subscribe((response) => {
+          const uuid = response.id;
+          this.cookie.set("uuid", String(uuid));
+          this.cookie.set("username", String(response.username));
+          this.router.navigate(['/feed']);
+        })
       }
     );
-
-    this.http.get<User>(`${this.usersUrl}/by-email/${login.email}`,{headers : this.options.headers}).subscribe((response) => {
-      // console.log(response);
-      console.log(response.id);
-      const uuid = response.id;
-      this.cookie.set("uuid", String(uuid));
-      this.cookie.set("username", String(response.username))
-    })
   }
 
 }
