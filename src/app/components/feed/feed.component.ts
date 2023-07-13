bcd0725ea0eae9f8a03fc072a69e62c4b6fc5b09
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-13 17:52:39 -0500

    Add validation and recommenderUsers list

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -3,8 +3,9 @@ import { Title } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 import { MenuItem } from 'primeng/api';
 import { timeInterval } from 'rxjs';
-import { Post } from 'src/app/models/types';
+import { Post, User } from 'src/app/models/types';
 import { PostService } from 'src/app/services/post.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-feed',
@@ -13,23 +14,29 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class FeedComponent {
 
-  items: MenuItem[] = [];
   font: any;
   posts : Post[] = [];
+  recommendedUsers : User[] = [];
   time = 0;
+  renderPlaceholder = false;
+  userId = Number(this.cookie.get('uuid'));
+  // skeletons : Number[] = [1,2,3];
 
-  constructor(private postDataB : PostService, private title : Title, private cookie : CookieService) {
+  constructor(private title : Title, private cookie : CookieService, private userService : UserService, private postService : PostService) {
 
-    this.postDataB.getPost(Number(cookie.get("uuid"))).pipe(timeInterval()).subscribe((posts) => {
-      this.time = posts.interval;
 
-      const skeleton = document.getElementById("skeleton-loader") as HTMLElement;
-      setTimeout(() => {
-        this.posts = posts.value;
-        this.posts.sort((a,b) => a.id! - b.id!);
-        skeleton.style.display = 'none'
-      }, this.time * 0.1);
+    this.postService.getPost(Number(cookie.get("uuid"))).subscribe((posts) => {
+      this.posts = posts;
+      this.posts.sort((a,b) => a.id! - b.id!);
+
+      this.posts.length > 0 ? this.renderPlaceholder = false : this.renderPlaceholder = true;
     })
+
+    if (!this.renderPlaceholder) {
+      this.userService.getAllUsers().subscribe((users) => {
+        this.recommendedUsers = users;
+      });
+    }
 
     this.title.setTitle("Feed")
 
