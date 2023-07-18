51f82691350919bf9d09852127d76b27d6d37fc7
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-17 20:01:10 -0500

    Create not logged component

---
+++ src/app/components/not-logged/not-logged.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-logged',
+  templateUrl: './not-logged.component.html',
+  styleUrls: ['./not-logged.component.css']
+})
+export class NotLoggedComponent {
+
+}
