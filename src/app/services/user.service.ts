7ae27dce3d5f658628e38b06fbdb2b8d5bfdcd6f
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-18 21:58:33 -0500

    Add user subject

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from '../models/types';
 
 @Injectable({
@@ -10,15 +11,24 @@ import { User } from '../models/types';
 export class UserService {
 
   private baseUrl  = 'http://localhost:8080/api/users';
+  users : User[] = [];
+  private userSubject: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  public users$: Observable<User[]> = this.userSubject.asObservable();
 
-  constructor(private http : HttpClient, private cookies : CookieService) { }
+  constructor(private http : HttpClient, private cookies : CookieService) {
+
+  }
 
   getUserProfile(id : number) : Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/${id}`);
   }
 
-  getAllUsers() : Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl)
+  getAllUsers() {
+    this.http.get<User[]>(`${this.baseUrl}`).pipe(
+      map((users: User[]) => {
+        this.userSubject.next(users);
+      })
+    ).subscribe();
   }
 
   createNewUser(user : User) : Observable<any> {
