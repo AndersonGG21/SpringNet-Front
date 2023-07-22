11a5e17d3b5fb7a49092b9ba495cf2062d84cc2c
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-22 17:25:19 -0500

    Refactor code

--- src/app/components/saved-posts/saved-posts.component.ts
+++ src/app/components/saved-posts/saved-posts.component.ts
@@ -9,43 +9,53 @@ import { PostService } from 'src/app/services/post.service';
 @Component({
   selector: 'app-saved-posts',
   templateUrl: './saved-posts.component.html',
-  styleUrls: ['./saved-posts.component.css']
+  styleUrls: ['./saved-posts.component.css'],
 })
 export class SavedPostsComponent implements OnInit {
-  posts : Post[] = [];
-  likedPosts : Post[] = [];
+  posts: Post[] = [];
+  likedPosts: Post[] = [];
   saved = false;
   renderPlaceholder = false;
   private loginService = inject(LoginService);
 
-  constructor(private postService : PostService, private cookie : CookieService, private aRoute : ActivatedRoute, private title : Title){}
+  constructor(
+    private postService: PostService,
+    private cookie: CookieService,
+    private aRoute: ActivatedRoute,
+    private title: Title
+  ) {}
 
   ngOnInit(): void {
     this.loginService.isLoggedIn();
 
-    this.aRoute.url.subscribe(url => {
-      if (url[0].path == "liked") {
-        this.postService.getLikedPosts(Number(this.cookie.get('uuid'))).subscribe((response) => {
-          this.title.setTitle(`${this.cookie.get('username')} | Liked Posts`);
-          this.likedPosts = response;
-          this.likedPosts.length > 0 ? this.renderPlaceholder = false : this.renderPlaceholder = true;
-        })
-      }else{
-        this.aRoute.paramMap.subscribe((params : ParamMap) => {
-          this.postService.getSavedPosts(Number(params.get('id'))).subscribe((response) => {
-            this.title.setTitle(`@${this.cookie.get('username')} | Saved Posts`);
+    this.aRoute.url.subscribe((url) => {
+      if (url[0].path == 'liked') {
+        this.postService
+          .getLikedPosts(Number(this.cookie.get('uuid')))
+          .subscribe((response) => {
+            this.title.setTitle(`${this.cookie.get('username')} | Liked Posts`);
+            this.likedPosts = response;
+            this.likedPosts.length > 0
+              ? (this.renderPlaceholder = false)
+              : (this.renderPlaceholder = true);
+          });
+      } else {
+        this.postService
+          .getSavedPosts(Number(this.cookie.get('uuid')))
+          .subscribe((response) => {
+            this.title.setTitle(
+              `@${this.cookie.get('username')} | Saved Posts`
+            );
             this.saved = true;
             for (let index = 0; index < response.length; index++) {
-              this.posts.push(response[index].post)
+              this.posts.push(response[index].post);
             }
 
-            this.posts.length > 0 ? this.renderPlaceholder = false : this.renderPlaceholder = true;
-          })
-        })
+            this.posts.length > 0
+              ? (this.renderPlaceholder = false)
+              : (this.renderPlaceholder = true);
+          });
       }
     });
-
-  };
-
-
+  }
 }
